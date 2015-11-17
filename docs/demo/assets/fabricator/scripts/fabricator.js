140487f7e9e4797054a8fae48a8f9520ff898086
Thomas Nägele <hello.thomas.naegele@gmail.com>
2015-11-17 17:56:51 +0100

    fix(demo): fixes #33. Change namespace inside demo scripts

--- docs/demo/assets/fabricator/scripts/fabricator.js
+++ docs/demo/assets/fabricator/scripts/fabricator.js
@@ -115,7 +115,7 @@ fabricator.setActiveItem = function () {
 			if (href.indexOf('#') > -1) {
 				id = href.split('#').pop();
 			} else {
-				id = href.split('/').pop().replace(/\.[^/.]+$bs-/, '');
+				id = href.split('/').pop().replace(/\.[^/.]+$dc-/, '');
 			}
 
 			items.push(id);
@@ -140,7 +140,7 @@ fabricator.setActiveItem = function () {
 		if (href.indexOf('#') > -1) {
 			id = window.location.hash.replace('#', '');
 		} else {
-			id = window.location.pathname.split('/').pop().replace(/\.[^/.]+$bs-/, '');
+			id = window.location.pathname.split('/').pop().replace(/\.[^/.]+$dc-/, '');
 		}
 
 		// In case the first menu item isn't the index page.
@@ -370,7 +370,7 @@ fabricator.setBreakpointsState = function ()
 	// Set initial state
 	if(localStorage.getItem("breakpoints") !== "shown")
 	{
-		root.classList.remove('bs-is-show-breakpoints');
+		root.classList.remove('dc-is-show-breakpoints');
 	}
 
 	// Breakpoint toggle
@@ -382,15 +382,15 @@ fabricator.setBreakpointsState = function ()
 		{
 			e.preventDefault();
 
-			if(root.classList.contains("bs-is-show-breakpoints"))
+			if(root.classList.contains("dc-is-show-breakpoints"))
 			{
 				localStorage.removeItem("breakpoints");
-				root.classList.remove('bs-is-show-breakpoints');
+				root.classList.remove('dc-is-show-breakpoints');
 			}
 			else
 			{
 				localStorage.setItem("breakpoints", "shown");
-				root.classList.add('bs-is-show-breakpoints');
+				root.classList.add('dc-is-show-breakpoints');
 			}
 		});
 	}
@@ -409,7 +409,7 @@ fabricator.setGridState = function ()
 	// Set initial state
 	if(localStorage.getItem("grid") !== "shown")
 	{
-		root.classList.remove('bs-is-show-grid');
+		root.classList.remove('dc-is-show-grid');
 	}
 
 	// Breakpoint toggle
@@ -421,15 +421,15 @@ fabricator.setGridState = function ()
 		{
 			e.preventDefault();
 
-			if(root.classList.contains("bs-is-show-grid"))
+			if(root.classList.contains("dc-is-show-grid"))
 			{
 				localStorage.removeItem("grid");
-				root.classList.remove('bs-is-show-grid');
+				root.classList.remove('dc-is-show-grid');
 			}
 			else
 			{
 				localStorage.setItem("grid", "shown");
-				root.classList.add('bs-is-show-grid');
+				root.classList.add('dc-is-show-grid');
 			}
 		});
 	}
