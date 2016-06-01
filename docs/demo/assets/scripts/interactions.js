ca942282647f04c9c1acd27a61cb7756ec2a373b
Gabriel Lovato <gabrielhl@gmail.com>
2016-06-01 18:48:18 +0200

    feat(search): Add clear icon for search.
    Add example behavior to search demo.
    Close #200.

--- docs/demo/assets/scripts/interactions.js
+++ docs/demo/assets/scripts/interactions.js
@@ -1,5 +1,7 @@
 // /* Demo Dropdowns and Dropups (button-dropdown.html) */
 $(document).ready(function() {
+// end
+
     $("#dropdown-example").click(function(e){
         e.preventDefault();
         $("#dropdown-example-list").toggle();
@@ -8,4 +10,23 @@ $(document).ready(function() {
         e.preventDefault();
         $("#dropup-example-list").toggle();
     });
+
+    $(".dc-search-form input").on("input",function(){
+        if($(this).val() != "") {
+            $(this).siblings(".dc-search-form__btn").find(".dc-icon").removeClass("dc-icon--search");
+            $(this).siblings(".dc-search-form__btn").find(".dc-icon").addClass("dc-icon--clear");
+        }
+        else {
+            $(this).siblings(".dc-search-form__btn").find(".dc-icon").addClass("dc-icon--search");
+            $(this).siblings(".dc-search-form__btn").find(".dc-icon").removeClass("dc-icon--clear");
+        }
+    });
+
+    $(".dc-search-form .dc-icon").click(function(){
+        $(this).parent().siblings(".dc-input").val("");
+        $(this).addClass("dc-icon--search");
+        $(this).removeClass("dc-icon--clear");
+    });
+
+// end
 });
